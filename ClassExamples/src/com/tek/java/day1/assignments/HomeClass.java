b3b4b0b48f689fbc95788ac1c4e45ca9b59874f0
caitlinnorse <caitenorse@gmail.com>
2023-02-06 19:19:31 -0500

    editing first activity 303.1.2 activity

--- ClassExamples/src/com/tek/java/day1/assignments/HomeClass.java
+++ ClassExamples/src/com/tek/java/day1/assignments/HomeClass.java
@@ -3,12 +3,22 @@ package com.tek.java.day1.assignments;
 
 public class HomeClass {
 	public static void main(String args[]) {
-		int var1 = 2;
-		int var2 = 5;
+		
+		// first activity
+		int var1;
+		int var2;
+		
+		var1 = 3;
+		var2 = 9;
 		
 		int sum = var1 + var2;
 		
 		System.out.println(sum);
+		
+		// second activity
+		
+		
+		
 	}
 
 }
