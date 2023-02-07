998fe998b184ef2f6572d1a9b18c69d027d275bf
caitlinnorse <caitenorse@gmail.com>
2023-02-07 10:27:12 -0500

    Cleanup

--- ClassExamples/src/com/tek/java/day2/labs/StringUpperExample.java
+++
@@ -1,8 +0,0 @@
-package com.tek.java.day2.labs;
-
-public class StringUpperExample{ 
-	  public static void main(String args[]){ 
-	    String s1="hello how are you"; 
-	    String s1upper=s1.toUpperCase(); 
-	    System.out.println(s1upper); 
-	}}
