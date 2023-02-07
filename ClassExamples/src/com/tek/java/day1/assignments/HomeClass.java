089e941c7973cfe17af3e74b87d5ddd0fe3cb88b
caitlinnorse <caitenorse@gmail.com>
2023-02-06 19:17:27 -0500

    Complete first activity

--- ClassExamples/src/com/tek/java/day1/assignments/HomeClass.java
+++ ClassExamples/src/com/tek/java/day1/assignments/HomeClass.java
@@ -3,7 +3,12 @@ package com.tek.java.day1.assignments;
 
 public class HomeClass {
 	public static void main(String args[]) {
+		int var1 = 2;
+		int var2 = 5;
 		
+		int sum = var1 + var2;
+		
+		System.out.println(sum);
 	}
 
 }
