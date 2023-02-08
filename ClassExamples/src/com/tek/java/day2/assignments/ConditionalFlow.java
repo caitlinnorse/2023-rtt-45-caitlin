e84c35b8aa54b40eabbec50fe0aa7e12f654c5a3
caitlinnorse <caitenorse@gmail.com>
2023-02-08 07:35:08 -0500

    Completed Eexercise 1 303.2.2

---
+++ ClassExamples/src/com/tek/java/day2/assignments/ConditionalFlow.java
@@ -0,0 +1,17 @@
+package com.tek.java.day2.assignments;
+
+public class ConditionalFlow {
+	public static void main (String args[]) {
+		// First exercise
+		
+		System.out.println("First Exercise\n");
+		
+		int x = 15;
+		
+		if ( x < 10) {
+			System.out.println("Less than 10");
+			
+		}
+	}
+
+}
