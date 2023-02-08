ec2ff45dfa6c4222f2ed5bc20815592a368d809a
caitlinnorse <caitenorse@gmail.com>
2023-02-08 08:00:27 -0500

    Completed exercise 6 Activity 303.2.2

--- ClassExamples/src/com/tek/java/day2/assignments/ConditionalFlow.java
+++ ClassExamples/src/com/tek/java/day2/assignments/ConditionalFlow.java
@@ -74,6 +74,31 @@ public class ConditionalFlow {
 		} else {
 			
 		}
+		
+		// Sixth exercise
+		System.out.println("\nSixth Exercise");
+		
+		System.out.println("Please enter a number corresponding to a day of the week.");
+		int weekday = scan.nextInt();
+		
+		switch(weekday) {
+		case 1: System.out.println("Monday");
+			break;
+		case 2: System.out.println("Tuesday");
+			break;
+		case 3: System.out.println("Wednesday");
+			break;
+		case 4: System.out.println("Thursday");
+			break;
+		case 5: System.out.println("Friday");
+			break;
+		case 6: System.out.println("Saturday");
+			break;
+		case 7: System.out.println("Sunday");
+			break;
+		default: System.out.println("Out of range");
+			break;
+		}
 	}
 
 }
