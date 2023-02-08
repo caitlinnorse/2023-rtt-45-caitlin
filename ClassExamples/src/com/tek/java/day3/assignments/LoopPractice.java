11a2394f7ea01a26eb18bb6c7372cc496b366c1f
caitlinnorse <caitenorse@gmail.com>
2023-02-08 16:31:35 -0500

    Completing exercise 3 loop practice activity

--- ClassExamples/src/com/tek/java/day3/assignments/LoopPractice.java
+++ ClassExamples/src/com/tek/java/day3/assignments/LoopPractice.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class LoopPractice {
 	public static void main(String args[]) {
 		// exercise 1
-		/*
 		System.out.println("Exercise 1");
 		for(int i = 1; i <= 12; i++) {
 			System.out.println(i + " ");
@@ -15,9 +14,11 @@ public class LoopPractice {
 				System.out.print(j * i + " ");
 			}
 		}
-		*/
-		/*
+		
+		
 		// exercise 2
+		System.out.println();
+		System.out.println("\nExercise 2");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter first number: ");
 		int n1 = scan.nextInt();
@@ -42,20 +43,24 @@ public class LoopPractice {
 		}
 		
 		System.out.println(maxVal);
-		*/
+		
 		
 		// exercise 3
+		System.out.println("\nExercise 3");
 		int tuition = 10000;
 		int doubleTuition = tuition * 2;
 		int newTuition = tuition;
 		int counter = 0;
 		
-		while(newTuition != doubleTuition) {
+		//System.out.println(doubleTuition);
+		//System.out.println(newTuition);
+		
+		while(newTuition < doubleTuition) {
 			newTuition *= 1.07;
 			counter++;
-			System.out.println(newTuition);
-		}
-		System.out.println(counter);
+			//System.out.println(newTuition);
+		} 
+		System.out.println("Your tuition will be doubled in " + counter + " years.");
 	}
 
 }
