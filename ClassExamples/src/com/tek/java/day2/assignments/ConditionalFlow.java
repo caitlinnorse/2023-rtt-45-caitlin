4f849db5132b7bb5c3d7e83d8ebfdaf614c6c27b
caitlinnorse <caitenorse@gmail.com>
2023-02-08 07:41:59 -0500

    Complete exercise 3 Activity 303.2.2

--- ClassExamples/src/com/tek/java/day2/assignments/ConditionalFlow.java
+++ ClassExamples/src/com/tek/java/day2/assignments/ConditionalFlow.java
@@ -4,17 +4,17 @@ public class ConditionalFlow {
 	public static void main (String args[]) {
 		// First exercise
 		
-		System.out.println("First Exercise\n");
+		System.out.println("First Exercise");
 		
 		int x = 15;
 		
 		if ( x < 10) {
-			System.out.println("Less than 10");
+			System.out.println("\nLess than 10");
 			
 		}
 		
 		// Second exercise
-		System.out.println("Second Exercise\n");
+		System.out.println("\nSecond Exercise");
 		
 		int y = 15;
 		
@@ -22,6 +22,19 @@ public class ConditionalFlow {
 			System.out.println("Less than 10");
 		} else {
 			System.out.println("Greater than 10");
+		}
+		
+		// Third exercise
+		System.out.println("\nThird Exercise");
+		
+		int z = 50;
+		
+		if (z < 10) {
+			System.out.println("Less than 10");
+		} else if (z >= 10 && z < 20) {
+			System.out.println("Between 10 and 20");
+		} else {
+			System.out.println("Greater than or equal to 20");
 		}
 	}
 
