1c9249c0865db00763244866e8f2830d7b71b9e2
caitlinnorse <caitenorse@gmail.com>
2023-02-08 20:28:15 -0500

    Complete exercise 1 arrays

---
+++ ClassExamples/src/com/tek/java/day2/assignments/Arrays.java
@@ -0,0 +1,19 @@
+package com.tek.java.day2.assignments;
+
+public class Arrays {
+	public static void main(String args[]) {
+		// exercise 1
+		System.out.println("Exercise 1");
+		
+		int[] ints = new int[3];
+		ints[0] = 1;
+		ints[1] = 2;
+		ints[2] = 3;
+		
+		System.out.println(ints[0]);
+		System.out.println(ints[1]);
+		System.out.println(ints[2]);
+		
+	}
+
+}
