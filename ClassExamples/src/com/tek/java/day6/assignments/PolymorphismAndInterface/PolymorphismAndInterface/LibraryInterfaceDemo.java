7e55cece79142d896537359ddb2e90deea258e53
caitlinnorse <caitenorse@gmail.com>
2023-02-13 16:54:51 -0500

    Complete 303.6.3

--- ClassExamples/src/com/tek/java/day6/assignments/PolymorphismAndInterface/PolymorphismAndInterface/LibraryInterfaceDemo.java
+++ ClassExamples/src/com/tek/java/day6/assignments/PolymorphismAndInterface/PolymorphismAndInterface/LibraryInterfaceDemo.java
@@ -4,6 +4,8 @@ public class LibraryInterfaceDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Test case 1
 		KidUser k = new KidUser();
 		k.setAge(10);
 		
@@ -21,6 +23,26 @@ public class LibraryInterfaceDemo {
 				
 		k1.setBookType("Fiction");
 		k1.requestBook();
+		
+		System.out.println("--------------------------------------------------------------");
+		
+		AdultUser a = new AdultUser();
+		a.setAge(5);
+		a.registerAccount();
+		
+		a.setBookType("Kids");
+		a.requestBook();
+		
+		System.out.println();
+		
+		AdultUser a1 = new AdultUser();
+		a1.setAge(23);
+		a1.registerAccount();
+		
+		a1.setBookType("Fiction");
+		a1.requestBook();
+		
+		
 	}
 
 }
